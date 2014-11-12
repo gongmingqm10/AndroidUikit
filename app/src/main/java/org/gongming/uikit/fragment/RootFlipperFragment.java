601e0a2e8e2a883638071835e3f973ef416302f8
gongming <gongmingqm10@foxmail.com>
2014-11-12 14:46:07 +0800

    add fontello icon support

--- app/src/main/java/org/gongming/uikit/fragment/RootFlipperFragment.java
+++ app/src/main/java/org/gongming/uikit/fragment/RootFlipperFragment.java
@@ -1,12 +1,16 @@
 package org.gongming.uikit.fragment;
 
+import android.annotation.TargetApi;
 import android.app.Fragment;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.PopupWindow;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -25,6 +29,7 @@ public class RootFlipperFragment extends Fragment {
 
         final TextView textView = ButterKnife.findById(view, R.id.textView);
         final Button button = ButterKnife.findById(view, R.id.button);
+        final Button testBtn = ButterKnife.findById(view, R.id.testBtn);
 
         CacheData<Integer> lastCached = CacheManager.getInstance().getCache(CacheUtils.KEY_TEST);
         if (lastCached != null) textView.setText("Last cache = " + lastCached.getData());
@@ -38,7 +43,34 @@ public class RootFlipperFragment extends Fragment {
                 Toast.makeText(getActivity(), "This number is " + number, Toast.LENGTH_SHORT).show();
             }
         });
+
+
+        initPopupWindow();
+
+
+        final PopupWindow window = new PopupWindow(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        window.setContentView(inflater.inflate(R.layout.popwindow, null));
+        testBtn.setOnClickListener(new View.OnClickListener() {
+            @TargetApi(Build.VERSION_CODES.KITKAT)
+            @Override
+            public void onClick(View v) {
+                if (window.isShowing()) {
+                    window.dismiss();
+                    return;
+                }
+                window.getContentView().measure(0, 0);
+                int xOffset =  testBtn.getMeasuredWidth() - window.getContentView().getMeasuredWidth();
+                window.showAsDropDown(testBtn, xOffset, 5);
+            }
+        });
+
+
         return view;
+    }
+
+    private void initPopupWindow() {
+
+
     }
 
 
